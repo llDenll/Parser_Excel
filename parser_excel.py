535c03d194c17a5e95178b595bde9dc99848f196
llDenll <Dens.030206@gmail.com>
2022-06-24 14:49:47 +0300

    Create parser_excel.py

---
+++ parser_excel.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import xlsxwriter
+
+url = 'https://wolf.ua/uk/razdely/poligrafiya/'
+rr = requests.get(url)
+
+soup = BeautifulSoup(rr.text, 'lxml')
+
+data = soup.find_all(class_='product-column x5 text-center')
+
+elemensts = []
+for i in data:
+    name = i.find(class_='product-name').text.strip()
+    img = 'https:' + i.find('img').get('src')
+    elemensts.append({
+        'name': name,
+        'img': img
+    })
+
+book = xlsxwriter.Workbook('ivan.xlsx')
+page = book.add_worksheet()
+
+row = 0
+colum = 0
+
+page.set_column('A:A', 20)
+page.set_column('B:B', 10)
+
+for i in elemensts:
+    page.write(row, colum, i['name'])
+    page.write(row, colum + 1, i['img'])
+    row += 1
+
+book.close()
